b5fa6b00b0afe49ef899047dd8c0db2ee93b4932
KostiaNetot <pugjazz@gmail.com>
2024-10-24 20:15:14 +0200

    revalidate formData

---
+++ app/lib/actions.ts
@@ -0,0 +1,37 @@
+'use server';
+
+import { z as zod } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = zod.object({
+  id: zod.string(),
+  customerId: zod.string(),
+  amount: zod.coerce.number(),
+  status: zod.enum(['pending', 'paid']),
+  date: zod.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  console.log('Revalidate formData: ', { customerId, amountInCents, status, date });
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
